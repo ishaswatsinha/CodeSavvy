174b76b5b54de93e089c37b43ccb091f31ce47f6
Shaswat <shaswatsinha87@gmail.com>
2025-02-15 15:31:37 +0530

    BasicTemplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Hey There!!! Shaswat here
+        </p> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
