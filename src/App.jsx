c5293ae8c5be8e3fc1789e41713b4744ad194832
Shaswat <shaswatsinha87@gmail.com>
2025-04-23 22:15:57 +0530

    conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,27 @@ import EditSetting from "./pages/StudentSetting";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Navbar from "./components/common/Navbar";
-import { useState } from "react";
+// import { useState } from "react";
 import ContactPage from "./pages/ContactUs";
 import ForgotPassword from "./pages/ForgotPassword";
 import OpenRoute from "./components/core/Auth/OpenRoute"
 import VerifyEmail from "./pages/VerifyEmail";
 import UpdatePassword from "./pages/UpdatePassword";
 
-import CourseDetails from "./pages/CourseDetails";
-import AddCourse from "./components/core/Dashboard/AddCourse/index";
+// import CourseDetails from "./pages/CourseDetails";
+// import AddCourse from "./components/core/Dashboard/AddCourse/index";
 // import MyCourses from "./components/core/Dashboard/MyCourses/MyCourses";
 // import EditCourse from "./components/core/Dashboard/EditCourse.jsx/EditCourse";
 // import InstructorDashboard from "./components/core/Dashboard/InstructorDashboard/InstructorDashboard";
 
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  // const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
-        <Route path="/About" element={<About />} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/About" element={<About />} />
         <Route
           path="login"
           element={
@@ -46,9 +45,8 @@ function App() {
             </OpenRoute>
           }
         />
-        <Route path="/StudentProfile" element={< ProfilePage/>} />
-        <Route path="/StudentSetting" element={< EditSetting/>} />
-        <Route path="/contact" element={<ContactPage/>}/>
+        {/* <Route path="/StudentProfile" element={< ProfilePage/>} /> */}
+        {/* <Route path="/StudentSetting" element={< EditSetting/>} /> */}
         <Route
           path="verify-email"
           element={
@@ -74,17 +72,13 @@ function App() {
             </OpenRoute>
           }
         />  
-        <Route
-          path="/signup"
-          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
-        />
         <Route path="/StudentProfile" element={<ProfilePage />} />
         <Route path="/StudentSetting" element={<EditSetting />} />
         <Route path="/contact" element={<ContactPage />} />
-        <Route path="/courses/:courseId" element={<CourseDetails />} />
+        {/* <Route path="/courses/:courseId" element={<CourseDetails />} /> */}
 
         <>
-          <Route path="dashboard/add-course" element={<AddCourse />} />
+          {/* <Route path="dashboard/add-course" element={<AddCourse />} /> */}
 
           {/* <Route path="dashboard/my-courses" element={<MyCourses />} /> */}
 
